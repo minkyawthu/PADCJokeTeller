9bdad8966c3d4b348f8ead6e9eab36480a304dc1
minkyawthu <compaqmkt@gmail.com>
2016-06-25 16:13:39 +0630

    initial project commit

---
+++ app/src/main/java/example/com/padcjoketeller/JokeTellerActivityFragment.java
@@ -0,0 +1,22 @@
+package example.com.padcjoketeller;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class JokeTellerActivityFragment extends Fragment {
+
+    public JokeTellerActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_joke_teller, container, false);
+    }
+}
